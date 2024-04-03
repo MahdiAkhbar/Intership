31e01e23a5755217f125b4f3de67c0460a1a54c6
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-03 16:16:25 +0330

    graph section layout created working on graph section header

--- src/app/components/home/graph/graph.component.ts
+++ src/app/components/home/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IGraphType } from '../../../shared/interfaces/graphType';
 
 @Component({
   selector: 'app-graph',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './graph.component.css'
 })
 export class GraphComponent {
+
+  graphType: IGraphType[] = [
+    { value: 'khatti-0', viewValue: 'خطی' },
+    { value: 'milei-1', viewValue: 'میله ای' },
+    { value: 'shami-2', viewValue: 'شمعی' }
+  ];
+  graphDuration: IGraphType[] = [
+    { value: 'shami-1', viewValue: 'امروز' },
+    { value: 'shami-2', viewValue: 'هفته جاری' },
+    { value: 'shami-3', viewValue: 'ماهانه' },
+    { value: 'shami-4', viewValue: 'سالانه' }
+  ];
+  graphTimeframe: IGraphType[] = [
+    { value: 'shami-1', viewValue: '۵ دقیقه‌ای' },
+    { value: 'shami-2', viewValue: '۱۵ دقیقه‌ای' },
+    { value: 'shami-3', viewValue: '۱ ساعت' }
+  ];
+
+  sellectedGraphType: string = this.graphType[0].value;
+  sellectedGraphDuration: string = this.graphDuration[0].value;
+  sellectedGraphTimeframe: string = this.graphTimeframe[0].value;
 
 }
