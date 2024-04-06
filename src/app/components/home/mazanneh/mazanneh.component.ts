3f9e4e302e8970370909c598a81b79b6d7bea3a2
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-06 11:38:37 +0330

    first state of mazanneh section created

--- src/app/components/home/mazanneh/mazanneh.component.ts
+++ src/app/components/home/mazanneh/mazanneh.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMazanneh } from '../../../shared/mazanneh';
 
 @Component({
   selector: 'app-mazanneh',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class MazannehComponent {
 
+  sellData: IMazanneh[] = [
+    {
+      count: 3,
+      volume: 2000000,
+      price: 5475
+    },
+    {
+      count: 2,
+      volume: 255000,
+      price: 5789
+    },
+    {
+      count: 20,
+      volume: 1000000,
+      price: 5475
+    },
+    {
+      count: 5,
+      volume: 400000,
+      price: 2045
+    },
+    {
+      count: 3,
+      volume: 700,
+      price: 78452
+    },
+  ];
+
+  columns: string[] = ['count', 'volume', 'price'];
+  sellHeaderColor: string = '#63E6BE';
+  buyHeaderColor: string = '#FFA8A8';
 }
