e18c2b208f9de8de62b6c876ace044053e38453f
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-07 10:09:54 +0330

    portfoy component

--- src/app/components/home/portfoy/portfoy.component.ts
+++ src/app/components/home/portfoy/portfoy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IPortfoy } from '../../../shared/interfaces/portfoy';
 
 @Component({
   selector: 'app-portfoy',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrl: './portfoy.component.css'
 })
 export class PortfoyComponent {
+  [x: string]: any;
+
+  portfoy: IPortfoy[] = [
+    {
+      name: 'شپنا',
+      count: 500,
+      lastPrice: 6000,
+      profitOrLoss: 14,
+      finishPrice: 6000,
+      change: {
+        value: 4.5,
+        viewValue: 4.5
+      },
+      currentValue: 5600,
+    },
+    {
+      name: 'ملت',
+      count: 500,
+      lastPrice: 6000,
+      profitOrLoss: -10,
+      finishPrice: 6000,
+      change: {
+        value: 0,
+        viewValue: 0
+      },
+      currentValue: 5600,
+    },
+    {
+      name: 'پلاسک',
+      count: 500,
+      lastPrice: 6000,
+      profitOrLoss: 0,
+      finishPrice: 6000,
+      change: {
+        value: 5,
+        viewValue: 5
+      },
+      currentValue: 5600,
+    },
+    {
+      name: 'شپنا',
+      count: 500,
+      lastPrice: 6000,
+      profitOrLoss: 14,
+      finishPrice: 6000,
+      change: {
+        value: -4.5,
+        viewValue: 4.5
+      },
+      currentValue: 5600,
+    }
+  ];
 
 }
