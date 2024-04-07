518e25d6e63813cabbe8ed7e78214198f5127355
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-07 16:59:56 +0330

    general files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/\material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InfoComponent } from './components/home/info/info.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,6 +18,9 @@ import { MazannehComponent } from './components/home/mazanneh/mazanneh.component
 import { GraphComponent } from './components/home/graph/graph.component';
 import { PortfoyComponent } from './components/home/portfoy/portfoy.component';
 import { WatchlistComponent } from './components/home/watchlist/watchlist.component';
+import { BuySellButtonsComponent } from './components/home/buy-sell-buttons/buy-sell-buttons.component';
+import { CategoryPipe } from './shared/pipes/category.pipe';
+import { SearchPipe } from './shared/pipes/search.pipe';
 
 @NgModule({
   declarations: [
@@ -22,12 +31,21 @@ import { WatchlistComponent } from './components/home/watchlist/watchlist.compon
     MazannehComponent,
     GraphComponent,
     PortfoyComponent,
-    WatchlistComponent
+    WatchlistComponent,
+    BuySellButtonsComponent,
+    CategoryPipe,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatInputModule
   ],
   providers: [
     provideAnimationsAsync()
