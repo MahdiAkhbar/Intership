d06a63b9e5ed106787f695555884fbde920cb4a5
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-09 15:05:23 +0330

    two component deleted

--- src/app/components/bottom-sheet/bottom-sheet.component.ts
+++ src/app/components/bottom-sheet/bottom-sheet.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { BuySellButtonsComponent } from '../home/buy-sell-buttons/buy-sell-buttons.component';
-import { BuySheetComponent } from './buy-sheet/buy-sheet.component';
-import { SellSheetComponent } from './sell-sheet/sell-sheet.component';
 
 @Component({
   selector: 'app-bottom-sheet',
@@ -11,13 +8,6 @@ import { SellSheetComponent } from './sell-sheet/sell-sheet.component';
 })
 export class BottomSheetComponent {
   constructor(private _bottomSheet: MatBottomSheet) { }
-  // goToOther(val: string) {
-  //   this._bottomSheet.dismiss();
-  //   if (val === '1')
-  //     this._bottomSheet.open(BuySheetComponent);
-  //   if (val === '2')
-  //     this._bottomSheet.open(SellSheetComponent);
-  // }
 
   orderType: string = 'buy';
 
