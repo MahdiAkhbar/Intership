110141582556ea85eb7f638ba154018054556146
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-09 08:35:49 +0330

    signup page created

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(private router: Router) { }
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    })
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
