60c9fc3f875fec0848d51ec86bfd586b71349484
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-09 13:30:53 +0330

    bottom sheet for buy and sell created

--- src/app/components/bottom-sheet/bottom-sheet.component.ts
+++ src/app/components/bottom-sheet/bottom-sheet.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BuySellButtonsComponent } from '../home/buy-sell-buttons/buy-sell-buttons.component';
+import { BuySheetComponent } from './buy-sheet/buy-sheet.component';
+import { SellSheetComponent } from './sell-sheet/sell-sheet.component';
 
 @Component({
   selector: 'app-bottom-sheet',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './bottom-sheet.component.css'
 })
 export class BottomSheetComponent {
+  constructor(private _bottomSheet: MatBottomSheet) { }
+  // goToOther(val: string) {
+  //   this._bottomSheet.dismiss();
+  //   if (val === '1')
+  //     this._bottomSheet.open(BuySheetComponent);
+  //   if (val === '2')
+  //     this._bottomSheet.open(SellSheetComponent);
+  // }
+
+  orderType: string = 'buy';
+
+  formType(val: string) {
+    this.orderType = val;
+  }
+
+  closeBottomSheet() {
+    this._bottomSheet.dismiss();
+  }
 
 }
