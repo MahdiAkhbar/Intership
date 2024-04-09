f3e86d9fe0339759e71a206f58bd03ce1ec6ec08
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-09 08:34:59 +0330

    login page created

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) { }
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required),
+      'remember': new FormControl(false)
+    })
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+  }
+
+  goToSignup() {
+    this.router.navigate(['/signup']);
+  }
 }
