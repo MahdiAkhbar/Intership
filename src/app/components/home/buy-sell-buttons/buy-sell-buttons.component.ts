17c2d5d351e9efe1c94ddd16fe429de2774e8982
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-14 13:58:42 +0330

    now we can change bottom sheet form type and class between sell and buy

--- src/app/components/home/buy-sell-buttons/buy-sell-buttons.component.ts
+++ src/app/components/home/buy-sell-buttons/buy-sell-buttons.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BottomSheetComponent } from '../../bottom-sheet/bottom-sheet.component';
+import { OrderService } from '../../../shared/services/order.service';
 
 @Component({
   selector: 'app-buy-sell-buttons',
@@ -8,9 +9,10 @@ import { BottomSheetComponent } from '../../bottom-sheet/bottom-sheet.component'
   styleUrl: './buy-sell-buttons.component.css'
 })
 export class BuySellButtonsComponent {
-  constructor(private _bottomSheet: MatBottomSheet,) { }
+  constructor(private _bottomSheet: MatBottomSheet, private orderService: OrderService) { }
 
-  openBottomSheet() {
+  async openBottomSheet(val: string) {
+    await this.orderService.setOrderType(val);
     this._bottomSheet.open(BottomSheetComponent);
   }
 
