98b0eadaac463e77d3b8ba1932b50a7c1144d05c
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-15 15:46:38 +0330

    login and signup components for desktop view

---
+++ src/app/components/desktop/d-login/d-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d-login',
+  templateUrl: './d-login.component.html',
+  styleUrl: './d-login.component.css'
+})
+export class DLoginComponent {
+
+}
---
+++ src/app/components/desktop/d-signup/d-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d-signup',
+  templateUrl: './d-signup.component.html',
+  styleUrl: './d-signup.component.css'
+})
+export class DSignupComponent {
+
+}
