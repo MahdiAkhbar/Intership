3bc1766ec9befde2f1f1b1ec8ac05ecffc43ace1
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-15 15:44:45 +0330

    separating mobile view and desktop view. desktop component created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserAgentService } from './shared/services/user-agent.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private userAgentService: UserAgentService, private location: Location) { }
   title = 'trader';
+
+  userDeviceType!: string;
+
+  ngOnInit(): void {
+    this.userDeviceType = this.userAgentService.getDeviceType();
+    if (this.userDeviceType === 'Desktop')
+      this.location.replaceState('/d');
+    else
+      this.location.replaceState('/m');
+  }
+
 }
