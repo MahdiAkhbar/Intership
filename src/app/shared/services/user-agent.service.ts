1022b33a845029592c56ccd96c3d727bd7b9b3af
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-15 16:25:15 +0330

    user agent service created in order to detect user device type

---
+++ src/app/shared/services/user-agent.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAgentService {
+
+  constructor() { }
+
+  getDeviceType() {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+      return 'Tablet';
+    } if (
+      /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+      return 'Mobile';
+    }
+    return 'Desktop';
+  }
+
+}
