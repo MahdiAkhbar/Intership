5ef4f06368235c576b366a770ec6a9ccde4ecddd
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-16 16:25:41 +0330

    --Desktop-- login view almost done

--- src/app/components/desktop/d-login/d-login.component.ts
+++ src/app/components/desktop/d-login/d-login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-d-login',
   templateUrl: './d-login.component.html',
   styleUrl: './d-login.component.css'
 })
-export class DLoginComponent {
+export class DLoginComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required),
+      'captcha': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    this.router.navigate(['/']);
+  }
+
+  goToSignup() {
+    this.router.navigate(['d', 'signup']);
+  }
 
 }
