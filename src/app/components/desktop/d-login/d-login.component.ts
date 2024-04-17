4dbb195522f67e16423ce48c7a5183defdc035cd
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-17 14:31:14 +0330

    login form validation

--- src/app/components/desktop/d-login/d-login.component.ts
+++ src/app/components/desktop/d-login/d-login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { passwordStrengthValidator } from '../../../shared/validators/passwordStrength.validator';
+import { minlengthValidator } from '../../../shared/validators/minlength.validator';
 
 @Component({
   selector: 'app-d-login',
@@ -15,13 +17,25 @@ export class DLoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'password': new FormControl(null, Validators.required),
+      'password': new FormControl(null, [Validators.required, passwordStrengthValidator(), minlengthValidator()]),
       'captcha': new FormControl(null, Validators.required)
     });
   }
 
   onSubmit() {
-    this.router.navigate(['/']);
+    console.log(this.loginForm.controls['password'].errors);
+    let formControls = this.loginForm.controls;
+    if (!formControls['username'].touched)
+      this.loginForm.controls['username'].markAsTouched();
+    if (!formControls['password'].touched)
+      this.loginForm.controls['password'].markAsTouched();
+    if (!formControls['captcha'].touched)
+      this.loginForm.controls['captcha'].markAsTouched();
+
+    if (this.loginForm.valid) {
+      this.loginForm.reset();
+      this.router.navigate(['/']);
+    }
   }
 
   goToSignup() {
