34334251beb2cdea9677c6664a4f8f9b5eb51d63
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-20 12:13:30 +0330

    [14030201]: web-view signup section created

--- src/app/components/web/d-signup/d-signup.component.ts
+++ src/app/components/web/d-signup/d-signup.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { passwordStrengthValidator } from '../../../shared/validators/passwordStrength.validator';
 
 @Component({
   selector: 'app-d-signup',
   templateUrl: './d-signup.component.html',
   styleUrl: './d-signup.component.css'
 })
-export class DSignupComponent {
+export class DSignupComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  signupForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, passwordStrengthValidator()])
+    })
+  }
+
+  onSubmit() {
+
+  }
+
+  goToLogin() {
+    this.router.navigate(['d/login']);
+  }
 
 }
