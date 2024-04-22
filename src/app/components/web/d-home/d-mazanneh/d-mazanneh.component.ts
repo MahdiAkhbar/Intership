07e86b8a51fef969854654c19953a92b3402e160
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-22 12:48:03 +0330

    [14030203]: web-view -> mazanneh component designed. also added styles from  mobile-view mazanneh component

--- src/app/components/web/d-home/d-mazanneh/d-mazanneh.component.ts
+++ src/app/components/web/d-home/d-mazanneh/d-mazanneh.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { IMazanneh } from '../../../../shared/interfaces/mazanneh';
 
 @Component({
   selector: 'app-d-mazanneh',
   templateUrl: './d-mazanneh.component.html',
-  styleUrl: './d-mazanneh.component.css'
+  styleUrls: ['../../../mobile/home/mazanneh/mazanneh.component.css', './d-mazanneh.component.css']
 })
 export class DMazannehComponent {
+
+  sellData: IMazanneh[] = [
+    {
+      count: 3,
+      volume: 2000000,
+      price: 5475
+    },
+    {
+      count: 2,
+      volume: 255000,
+      price: 5789
+    },
+    {
+      count: 20,
+      volume: 1000000,
+      price: 5475
+    },
+    {
+      count: 5,
+      volume: 400000,
+      price: 2045
+    },
+    {
+      count: 3,
+      volume: 700,
+      price: 78452
+    },
+  ];
+
+  columns: string[] = ['count', 'volume', 'price'];
+  sellHeaderColor: string = '#63E6BE';
+  buyHeaderColor: string = '#FFA8A8';
 
 }
