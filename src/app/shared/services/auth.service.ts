1f8675120a9583fe701f110e11e53a90a9622312
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-04-30 16:12:04 +0330

    [14030211]: login and signup error handling

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { ILogin } from '../interfaces/loginform.interface';
 import { ISignup } from '../interfaces/signupform.interface';
+import { catchError, tap, throwError } from 'rxjs';
+
+interface ILoginResponse {
+  refreshToken: string;
+  token: string;
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +20,22 @@ export class AuthService {
   ) { }
 
   signup(signupData: ISignup) {
-    return this.http.post(this.apiUrl + 'user/register', signupData);
+    return this.http.post(this.apiUrl + 'user/register', signupData).pipe(
+      catchError(errorRes => this.handleError(errorRes))
+    )
   }
 
   login(loginData: ILogin) {
-    return this.http.post(this.apiUrl + '/login', loginData);
+    return this.http.post<ILoginResponse>(this.apiUrl + '/login', loginData).pipe(
+      catchError(errorRes => this.handleError(errorRes))
+    )
   }
 
+  handleError(err: any) {
+    let errorMessage = 'مشکلی پیش آمد لطفا دوباره تلاش کنید';
+    if (!err.error) {
+      return throwError(() => errorMessage);
+    }
+    return throwError(() => err.error.msg);
+  }
 }
