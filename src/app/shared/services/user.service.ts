1d1b4ca6db69438b6d3333190c6e20cde9cec86c
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-05 14:22:16 +0330

    [14030216]: user service created and some methods created

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { IUser } from '../interfaces/user.interface';
+import { catchError, tap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient,
+    @Inject('API_URL') private apiUrl: string,
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  getUserProfile(username: string) {
+    return this.http.get<IUser>(this.apiUrl + '/user/' + username, { observe: 'response' }).pipe(
+      tap(res => {
+        this.setUser(<IUser>res.body)
+      })
+    );
+  }
+
+  setUser(userObj: IUser) {
+    localStorage.setItem('user', JSON.stringify(userObj));
+  }
+
+  getUser() {
+    return JSON.parse(<string>localStorage.getItem('user'));
+  }
+
+}
