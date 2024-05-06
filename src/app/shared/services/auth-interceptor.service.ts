34967cd6165d49a103579fbb71384176f9a9e4c1
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-06 13:36:10 +0330

    [14030217]: handle refresh token in interceptor

--- src/app/shared/services/auth-interceptor.service.ts
+++ src/app/shared/services/auth-interceptor.service.ts
@@ -1,24 +1,63 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs/internal/Observable';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { ILogin } from '../interfaces/loginform.interface';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient,
+    @Inject('API_URL') private apiUrl: string
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     const token = this.authService.getToken();
+
     if (!token)
       return next.handle(req);
 
-    // const haveAuth = req.headers.get('Authorization');
-    // if (haveAuth)
-    //   return next.handle(req);
+    const haveAuth = req.headers.get('Authorization');
+    if (haveAuth)
+      return next.handle(req);
 
-    const authReq = req.clone({ setHeaders: { Authorization: token } });
-    return next.handle(authReq);
+    const newReq = this.addTokenHeader(req, token);
+    return next.handle(newReq).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401 && this.authService.getRefreshToken())
+          return this.handle401Error(req, next);
+        return throwError(() => err);
+      })
+    )
+
   }
+
+  addTokenHeader(req: HttpRequest<any>, token: string) {
+    return req.clone({
+      setHeaders: {
+        Authorization: token
+      }
+    });
+  }
+
+  handle401Error(req: HttpRequest<any>, next: HttpHandler) {
+    return this.authService.refreshToken().pipe(
+      switchMap(() => {
+        const newToken = this.authService.getToken();
+        if (newToken) {
+          req = this.addTokenHeader(req, newToken);
+          return next.handle(req);
+        } else {
+          this.authService.logout();
+          return throwError(() => 'Failed to refresh token');
+        }
+      })
+    )
+  }
+
 }
