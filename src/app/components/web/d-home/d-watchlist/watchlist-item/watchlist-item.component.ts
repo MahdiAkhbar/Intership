3c7925f3a561b009e60fa9560f7dc14b142994eb
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-12 16:09:19 +0330

    [14030223]: watchlist remove button

--- src/app/components/web/d-home/d-watchlist/watchlist-item/watchlist-item.component.ts
+++ src/app/components/web/d-home/d-watchlist/watchlist-item/watchlist-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IWatchlist } from '../../../../../shared/interfaces/watchList';
+import { WatchListService } from '../../../../../shared/services/watch-list.service';
 
 @Component({
   selector: 'app-watchlist-item',
@@ -7,7 +8,18 @@ import { IWatchlist } from '../../../../../shared/interfaces/watchList';
   styleUrl: './watchlist-item.component.css'
 })
 export class WatchlistItemComponent {
+  constructor(
+    private watchlistService: WatchListService
+  ) { }
 
   @Input('watchList') item!: IWatchlist;
   @Input('index') index!: number;
+
+  onWatchlistRemove(e: Event, item: IWatchlist) {
+    e.stopPropagation();
+    this.watchlistService.removeWatchlistItem(this.item.insCode).subscribe(() => {
+      this.watchlistService.removeWatchListSubject.next(this.item);
+    })
+
+  }
 }
