9e6b0d6c438b881117268f92fecd2e50e0068e55
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-12 10:54:29 +0330

    [14030223]: emit insCode on desktop conponent init and subscribe to it everywhere we need

--- src/app/components/web/desktop.component.ts
+++ src/app/components/web/desktop.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from '../../shared/services/stock.service';
 
 @Component({
   selector: 'app-desktop',
   templateUrl: './desktop.component.html',
   styleUrl: './desktop.component.css'
 })
-export class DesktopComponent {
+export class DesktopComponent implements OnInit {
+  constructor(
+    private stockService: StockService
+  ) { }
+
+  ngOnInit(): void {
+    let ins = this.stockService.getInsCode();
+    this.stockService.insCode.next(ins);
+  }
 
 }
