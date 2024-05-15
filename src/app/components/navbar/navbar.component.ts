c50db15be5ad4bb47679595bd3b9479f03d0ed38
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-15 12:09:06 +0330

    [14030226]: mobile-view -> logout user on click logout button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  onLogout() {
+    this.authService.logout();
+  }
 
 }
