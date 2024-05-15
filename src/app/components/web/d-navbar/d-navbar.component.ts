6c6cd9ad90d0ed129e84aec7a477cac4f7950373
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-15 16:10:43 +0330

    [14030226]: web-view -> user firstname and lastname on navbar

--- src/app/components/web/d-navbar/d-navbar.component.ts
+++ src/app/components/web/d-navbar/d-navbar.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { UserService } from '../../../shared/services/user.service';
+import { IUser } from '../../../shared/interfaces/user.interface';
 
 @Component({
   selector: 'app-d-navbar',
   templateUrl: './d-navbar.component.html',
   styleUrl: './d-navbar.component.css'
 })
-export class DNavbarComponent {
-  constructor(private AuthService: AuthService, private router: Router) { }
+export class DNavbarComponent implements OnInit {
+  constructor(
+    private AuthService: AuthService,
+    private userService: UserService
+  ) { }
+
+  user: IUser = {
+    id: 0,
+    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    roles: [],
+    favorites: [],
+    notes: []
+  };
+
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
+  }
 
   logout() {
     this.AuthService.logout();
