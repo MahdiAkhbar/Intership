07f1364c3747467b23686a792348edc357e739d7
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-25 13:11:35 +0330

    [14030305]: web-view -> changed routes to just web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/mobile/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { MTradeComponent } from './components/mobile/home/m-trade/m-trade.component';
-import { AppComponent } from './app.component';
 import { DTradeComponent } from './components/web/d-trade/d-trade.component';
 import { DHomeComponent } from './components/web/d-home/d-home.component';
 import { DLoginComponent } from './components/web/d-login/d-login.component';
 import { DSignupComponent } from './components/web/d-signup/d-signup.component';
 import { authGuard } from './shared/guards/auth.guard';
+import { DesktopComponent } from './components/web/desktop.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent, pathMatch: 'full' },
-
   {
-    path: 'm', component: HomeComponent, canActivate: [authGuard], children: [
-      { path: '', component: MTradeComponent },
-      { path: 'profile', component: ProfileComponent },
+    path: '', component: DesktopComponent, children: [
+      { path: '', redirectTo: 'd', pathMatch: 'full' },
+      {
+        path: 'd', component: DHomeComponent, canActivate: [authGuard], children: [
+          { path: '', component: DTradeComponent },
+          { path: 'profile', component: ProfileComponent },
+        ]
+      },
     ]
   },
-  { path: 'm/signup', component: SignupComponent },
-  { path: 'm/login', component: LoginComponent },
 
-  {
-    path: 'd', component: DHomeComponent, canActivate: [authGuard], children: [
-      { path: '', component: DTradeComponent },
-      { path: 'profile', component: ProfileComponent },
-    ]
-  },
   { path: 'd/signup', component: DSignupComponent },
   { path: 'd/login', component: DLoginComponent },
   { path: '**', redirectTo: '' },
