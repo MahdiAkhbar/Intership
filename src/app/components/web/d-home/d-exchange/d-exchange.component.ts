f4bc88b0cb8df8104be8685baa6613ce5d62ca5d
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-27 09:54:06 +0330

    [14030307]: web-view -> now  inc and dec buttons are working and change the value of inputs

--- src/app/components/web/d-home/d-exchange/d-exchange.component.ts
+++ src/app/components/web/d-home/d-exchange/d-exchange.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { OrderService } from '../../../../shared/services/order.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-d-exchange',
@@ -8,19 +7,71 @@ import { OrderService } from '../../../../shared/services/order.service';
   styleUrl: './d-exchange.component.css'
 })
 export class DExchangeComponent {
-  constructor(private _bottomSheet: MatBottomSheet, private orderService: OrderService) { }
+  constructor() { }
 
-  orderType: string = 'buy';
+  isBuyMode: boolean = true;
+
+  tradeForm!: FormGroup;
 
   ngOnInit(): void {
+    this.tradeForm = new FormGroup({
+      price: new FormControl(null),
+      count: new FormControl(null),
+      totalPrice: new FormControl(null)
+    })
   }
 
-  formType(val: string) {
-    this.orderType = val;
+  formType(val: boolean) {
+    this.isBuyMode = val;
   }
 
-  closeBottomSheet() {
-    this._bottomSheet.dismiss();
+  increasePrice() {
+    if (!this.tradeForm.controls['price'])
+      this.tradeForm.patchValue({
+        price: 100
+      });
+    else
+      this.tradeForm.patchValue({
+        price: this.tradeForm.get('price')?.value + 100
+      });
+  }
+
+  decreadePrice() {
+    if (!this.tradeForm.get('price')?.value)
+      return;
+    else if (this.tradeForm.get('price')?.value < 100)
+      this.tradeForm.patchValue({
+        price: 0
+      });
+    else
+      this.tradeForm.patchValue({
+        price: this.tradeForm.get('price')?.value - 100
+      });
+  }
+
+  changecount(val: number) {
+    if (val < 0) {
+      if (!this.tradeForm.get('count')?.value)
+        return;
+      if (this.tradeForm.get('count')?.value <= Math.abs(val))
+        this.tradeForm.patchValue({
+          count: 0
+        });
+      else
+        this.tradeForm.patchValue({
+          count: this.tradeForm.get('count')?.value + val
+        });
+    }
+    else {
+      if (!this.tradeForm.get('count')?.value)
+        this.tradeForm.patchValue({
+          count: val
+        });
+      else
+        this.tradeForm.patchValue({
+          count: this.tradeForm.get('count')?.value + val
+        });
+    }
   }
 
 }
